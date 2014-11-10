6123063e65d3bfe3ffccc709ef750af51a697c11
andres-s <a.sarnari@gmail.com>
2014-11-10 23:45:26 +1100

    Sanitising comments somewhat sensibly

--- sasmacro.py
+++ sasmacro.py
@@ -1,5 +1,13 @@
+import re
 
 class SASMacro:
+
+    # Note that regexes that are truncations of other regexes need to appear
+    # later in the alternatives list, eg.
+    #   re.search(r'a|ab', 'ab').group() -> 'a'
+    #   re.search(r'ab|a', 'ab').group() -> 'ab'
+    _COMMENT_NOISE_RE = re.compile(r'^\s*\*\*\s*--+\s*|^\s*--+$|^\s*/\*-*|^\s*\*+[ \t]*|^\s+', #|^\s*-+\s*$|
+                                   re.MULTILINE)
 
     def __init__(self, name, comment,
                  position_params=None, keyword_params=None):
@@ -11,4 +19,9 @@ class SASMacro:
     def make_reST(self):
         return self._name + '\n' + \
                ('-'*len(self._name)) + '\n' + \
-               self._comment
+               self._sanitise_comment() if self._comment is not None else ''
+
+
+    def _sanitise_comment(self):
+        return self._COMMENT_NOISE_RE.sub('', self._comment)
+
